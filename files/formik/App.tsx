d0c53be37f5f800b170dec992cf07b89b3a36fe9
cerico <garethrobertlee@gmail.com>
2024-07-13 15:11:54 +0100

    feat: add contexts to formik role

--- files/formik/App.tsx
+++ files/formik/App.tsx
@@ -1,11 +1,24 @@
+import { MainProvider } from "./contexts"
 import FormPage from './components/FormPage'
+import Switch from './components/Switch'
 import './App.css'
 
 function App() {
 
   return (
-    <FormPage />
+    <>
+      <Switch />
+      <FormPage />
+    </>
   )
 }
 
-export default App
+function Root() {
+  return (
+    <MainProvider>
+      <App />
+    </MainProvider>
+  )
+}
+
+export default Root
---
+++ files/formik/contexts/index.tsx
@@ -0,0 +1,25 @@
+import { createContext, useState, useContext, ReactNode } from 'react'
+
+interface MainContextType {
+  theme: string
+  setTheme: (theme: string) => void
+}
+
+const MainContext = createContext<MainContextType>({
+  theme: "light",
+  setTheme: () => { },
+})
+
+
+const MainProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState("light")
+  return (
+    <MainContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </MainContext.Provider>
+  )
+}
+
+const useTheme = () => useContext(MainContext)
+
+export { MainProvider, useTheme }
